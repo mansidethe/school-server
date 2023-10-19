70e7cdb09f6fafc7e33b7315542c1c762124a0d7
mansidethe <mansidethe18@gmail.com>
2023-10-19 22:38:53 +0530

    school server

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ import express from 'express'
 
 const app = express();
 
+app.use(express.json)
 
+app.get('/health',(req, res)=>{
+    res.json({
+        status:'all good all set'
+    })
+})
 
-const PORT=5000;
+app.get('/students',(req,res)=>{
+    res.json({
+      sucess: true,
+      data:students,
+      message: 'successfully fetch on students'
+    })
+});
+
+const PORT=8080;
  
 app.listen((PORT),(
     console.log(`Server is running on port ${PORT}`)
